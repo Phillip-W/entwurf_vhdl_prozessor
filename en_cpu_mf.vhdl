b6b40bca9b2b6d798e44f2b5efbf054211a8beb2
flomaurer <flomaurer@users.noreply.github.com>
2015-05-15 11:02:16 +0200

    Update en_cpu_mf.vhdl

--- en_cpu_mf.vhdl
+++ en_cpu_mf.vhdl
@@ -7,4 +7,4 @@ ENTITY System IS
     clk, res, c_m_io, c_wa, c_io, i_sr : IN bit;            --1.2.1, 1.2.2, 1.2.6, 1.2.7, 1.2.8, 1.2.10
     dbus_m_io, dbus_mem, abus_mem : IN positive;            --1.2.3, 1.2.4, 1.2.5
     d_io : OUT bit);
-END CPU;
+END System;
