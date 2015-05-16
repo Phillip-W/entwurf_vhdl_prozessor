1487ab3d6427496bedf8086a9d7d656f96b1fa90
flomaurer <flomaurer@users.noreply.github.com>
2015-05-16 16:33:02 +0200

    Create pack_mem_defs_mf.vhdl

---
+++ pack_mem_defs_mf.vhdl
@@ -0,0 +1,12 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+PACKAGE mem_defs_pack IS
+  
+  --Hier alle Konstanten definieren - falls nötig
+  
+  --Hier alle Datentypen, Subtyps definieren - falls nötig
+  
+  --Hier alle nötigen Funktionen angeben
+  
+END mem_defs_pack;
