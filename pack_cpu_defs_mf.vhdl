6d122c93250da1ace40140b6ba1fa767fd50daae
flomaurer <flomaurer@users.noreply.github.com>
2015-05-16 16:12:46 +0200

    Create pack_cpu_defs_mf.vhdl

---
+++ pack_cpu_defs_mf.vhdl
@@ -0,0 +1,7 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+PACKAGE cpu_defs_pack IS
+  constant data_width       :positive :=12;   --1.1.1
+  constant address_width    :positive :=12;   --1.1.2
+END cpu_defs_pack;
