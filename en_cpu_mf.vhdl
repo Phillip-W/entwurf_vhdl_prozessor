f99921688c774aae32c90a3e41ab05eb77147224
flomaurer <flomaurer@users.noreply.github.com>
2015-05-18 22:04:11 +0200

    Update en_cpu_mf.vhdl

--- en_cpu_mf.vhdl
+++ en_cpu_mf.vhdl
@@ -6,6 +6,6 @@ ENTITY CPU IS
     address_width : positive); -- Parameter wieder auslagern.  1.1.2
   PORT(
     clk, res, c_m_io, c_wa, c_io, i_sr : IN bit;            --1.2.1, 1.2.2, 1.2.6, 1.2.7, 1.2.8, 1.2.10
-    dbus_m_io, dbus_mem, abus_mem : IN positive;            --1.2.3, 1.2.4, 1.2.5
+    dbus_m_io, dbus_mem, abus_mem : IN bit_vector;            --1.2.3, 1.2.4, 1.2.5
     d_io : OUT bit);
 END CPU;
