67f7a8f485bcf36a8cdd05e82a50d735de72b90e
leizhangde <leizhang.de@hotmail.com>
2015-05-20 19:30:22 +0200

    Update subtype_package_all.vhdl

--- subtype_package_all.vhdl
+++ subtype_package_all.vhdl
@@ -1,5 +1,6 @@
 package subtype_package_all is
      
-       subtype mem_data is integer range 4095 downto 0;  //subtype für Spericher
+      subtype mem_data is integer range 4095 downto 0;  //Subtype für Speicher
+      type mem_type is array(integer range 4095 downto 0) of integer range 4095 downto 0;  //type für Speicher (Array type)
        
 end subtype_package_all;
