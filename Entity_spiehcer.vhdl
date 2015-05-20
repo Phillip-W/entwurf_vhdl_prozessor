2a74604e27748052f3dd19ffa832a3bf04c34a24
leizhangde <leizhang.de@hotmail.com>
2015-05-20 19:41:20 +0200

    Update Entity_spiehcer.vhdl

--- Entity_spiehcer.vhdl
+++ Entity_spiehcer.vhdl
@@ -1,1 +1,21 @@
 use subtype_package_all.all;
+
+entity Speicher is  //Entity für Speicher
+   port(
+        clk : in boolean;   //Clock
+        addr: in mem_data;   // addr  datatyp ist von Subtype_package_all deklariert
+        data_in: in mem_data;   //Data.input
+        data_out: out mem_data);  //Data.output
+end Speicher;
+
+architecture function_S of Speicher is        
+begin
+    process( clk , addr , data_in)    //empfindliche Signal
+    variable Mem: mem_type;           // typ mem_type ist von subtype_package_all deklariert
+    begin
+       if clk then                    // wie Speicher funktioniert.
+          Mem(addr) := data_in;     
+       end if; 
+    data_out <= Mem(addr);
+    end process;
+end function_S;
