4aa0940f3d301e3e644e59b3b4137015195120e8
leizhangde <leizhang.de@hotmail.com>
2015-05-20 18:47:12 +0200

    Update subtype_package_all.vhdl

--- subtype_package_all.vhdl
+++ subtype_package_all.vhdl
@@ -1,5 +1,5 @@
 package subtype_package_all is
      
-       subtype mem_data in integer range 4095 downto 0;  //subtype für Spericher
+       subtype mem_data is integer range 4095 downto 0;  //subtype für Spericher
        
 end subtype_package_all;
