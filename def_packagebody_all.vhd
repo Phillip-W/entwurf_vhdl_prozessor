8fe94fe0099298010f2fc835a1971057d455bf6f
flomaurer <flo.maurer@tum.de>
2015-05-23 18:41:28 +0200

    neuer OPCode

--- def_packagebody_all.vhd
+++ def_packagebody_all.vhd
@@ -4,6 +4,12 @@ package body def_package_all is
 		return addr_type is
 		begin
 			return (PC+1)mod 2**addr_width;				  -- Überlauf unseres PC vermeiden
-    end INC;  
+    end INC; 
+
+	function "NOT" (constant A:data_type)
+		return data_type is
+		begin
+		return -A -1 +2**data_width;
+	end "NOT";
       
 end def_package_all;
