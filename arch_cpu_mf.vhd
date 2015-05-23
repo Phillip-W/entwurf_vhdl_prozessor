d6e799a02558833cafc87a05737bc3a1816d931e
flomaurer <flo.maurer@tum.de>
2015-05-23 13:31:12 +0200

    Kommentare

--- arch_cpu_mf.vhd
+++ arch_cpu_mf.vhd
@@ -7,18 +7,19 @@ ARCHITECTURE behav OF CPU IS
     Process 
       -- Speicher
       variable Memory: mem_type := init_memory; -- Speicher mit init_memory initialisieren
-      variable Reg   :reg_type;			-- TEST
-      variable Instr :data_type;
-      variable OP    :opcode_type;
-      variable x,y,z :reg_addr_type;
-      variable PC    :addr_type:=0;
+      variable Reg   :reg_type;			-- bisher nicht verwendet (wird für die Operationen benötigt)-- TEST
+      variable Instr :data_type;		-- Aus dem Speicher geholte Anweisung
+      variable OP    :opcode_type;		-- aus der Instr errechneter OPCode (ableich mit unserem def_pack)
+      variable x,y,z :reg_addr_type;		-- Registeradressen zur Verarbeitung unserer Anweisungen
+      variable PC    :addr_type:=0;		-- unser Prozesscounter
       
       begin
         Instr := Memory(PC); OP:= Instr / (2**reg_addr_width)**3; -- Anweisung lesen
         x:=(Instr / (2**reg_addr_width)**2)mod 2**reg_addr_width; -- Anweisung zerlegen
         y:= (Instr / 2** reg_addr_width) mod 2** reg_addr_width;  -- Anweisung zerlegen
         z:= Instr mod 2** reg_addr_width;                         -- Anweisung zerlegen
-        if PC=address_width - 1 
+
+        if PC=address_width - 1 				  -- Überlauf unseres PC vermeiden
           then PC :=0;
           else PC:=PC+1;
         end if;
