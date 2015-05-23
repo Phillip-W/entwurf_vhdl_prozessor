c7f00c2282f1fff60b6949bee5bd415eae16450d
flomaurer <flo.maurer@tum.de>
2015-05-23 13:40:16 +0200

    Kommentare

--- def_package_all.vhd
+++ def_package_all.vhd
@@ -13,17 +13,17 @@ package def_package_all is
   
   
   --Hier alle Datentypen, Subtyps definieren
-  subtype addr_type IS
+  subtype addr_type IS					-- unsers Adressen zum ansprechen des Speichers, z.B. durch den PC
     natural range 0 to 2**address_width-1;
-  subtype opcode_type IS
+  subtype opcode_type IS				-- für unsere OPCode Deklarationen
     natural range 0 to 2**opcode_width-1;
-  subtype reg_addr_type is
+  subtype reg_addr_type is				-- zum Ansprechen unserer Register
     natural range 0 to 2**reg_addr_width-1;  
-  subtype data_type is 
+  subtype data_type is 					-- Typ, wie unsere Anweisungen, etc. im Speicher abgelegt werden
     natural range 0 to 2**data_width-1;  
-  type mem_type is 
-    array(addr_type) of data_type;  --type für Speicher (Array type)
-  type reg_type is array(reg_addr_type) of data_type;
+  type mem_type is 					-- unser "Speicher" (Array)
+    array(addr_type) of data_type;  			
+  type reg_type is array(reg_addr_type) of data_type;	-- unsere "Register" (Array)
   
   --Hier alle OPCodes definieren
   constant code_nop  : opcode_type:=0;       --3.3.1.1
