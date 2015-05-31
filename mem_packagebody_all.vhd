fbf66d720c27c486cd46a31f3261b1bdc83f09ab
leizhangde <leizhang.de@hotmail.com>
2015-05-31 13:12:31 +0200

    Update mem_packagebody_all.vhd

--- mem_packagebody_all.vhd
+++ mem_packagebody_all.vhd
@@ -8,6 +8,13 @@ package body mem_package_all is
       return -- hier unseren Ablauf eingeben (Befehle, wie sie der reihe nach ausgeführt werden sollen (immer mit * (2**reg_addr_width)**3 hinten dran
           (   0 => code_nop * (2**reg_addr_width)**3,
               1 => code_stop * (2**reg_addr_width)**3,
+              13 => code_sll * (2**reg_addr_width)**3,
+			        14 => code_srl * (2**reg_addr_width)**3,
+			        15 => code_sra * (2**reg_addr_width)**3,
+			        16 => code_rol * (2**reg_addr_width)**3,
+			        17 => code_rolc * (2**reg_addr_width)**3,
+			        18 => code_ror * (2**reg_addr_width)**3,
+			        19 => code_rorc * (2**reg_addr_width)**3,
               others => 0);
   end init_memory;        
 end mem_package_all;
