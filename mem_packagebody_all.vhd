0cf19aa9d4f856a866039a591368cb35da0d7279
leizhangde <leizhang.de@hotmail.com>
2015-06-05 14:42:03 +0200

    Update mem_packagebody_all.vhd

--- mem_packagebody_all.vhd
+++ mem_packagebody_all.vhd
@@ -10,22 +10,22 @@ package body mem_package_all is
 	     	 variable v   : data_type;
 		     variable i   : addr_type := 0 ;
          begin 
-	         outest: loop   ----- read line by line
-	            exit when endfile  (Memoryfile);
-		          readline (f , ml);
-          
-              success := TRUE ;
-              ----read values in each line
-              while success loop 
-                   read (ml, v, success);
-			             if success then
-                        Memory (i) := v ;
-                        exit outest when
-				                    i := 2**addr_width -1;
-				                i := i + 1;
-			             end if;
-		          end loop;
-		       end loop;
+         outest: loop   ----- read line by line
+         exit when endfile  (Memoryfile);
+         readline (f , ml);
+         
+         success := TRUE ;
+         ----read values in each line
+         while success loop
+             read (ml, v, success);
+	     if success then
+	        Memory (i) := v ;
+                exit outest when
+	                i := 2**addr_width -1;
+		i := i + 1;
+	      end if;
+          end loop;
+        end loop;
   end init_memory;
 				    
 end mem_package_all;
