62a59cc6aaff11c7ac3693ec0adff4f7d48cf1c7
leizhangde <leizhang.de@hotmail.com>
2015-06-05 14:33:33 +0200

    Update mem_package_all.vhd

--- mem_package_all.vhd
+++ mem_package_all.vhd
@@ -3,5 +3,7 @@ USE IEEE.std_logic_1164.ALL;
 Use work.def_package_all.all;
 
 package mem_package_all is
-  function init_memory return mem_type;     -- Funktion um unseren Speicher zu beschreiben 
+         file MemoryFile   :   Text is in "Memory";
+	       procedure init_memory( variable Memoryfile  : in text;
+			                           variable Memory: out mem_type) ; ---Funktion um unseren Speicher zu beschreiben 
 end mem_package_all;
