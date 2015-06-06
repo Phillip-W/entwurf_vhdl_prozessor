86f892883c413d59b042a05c6fdf0c9008d60f3b
leizhangde <leizhang.de@hotmail.com>
2015-06-06 16:11:34 +0200

    Update mem_packagebody_all.vhd

--- mem_packagebody_all.vhd
+++ mem_packagebody_all.vhd
@@ -3,26 +3,29 @@ USE IEEE.std_logic_1164.ALL;
 Use work.def_package_all.all;
 
 package body mem_package_all is
-  procedure init_memory( variable Memoryfile  : in text; variable Memory: out mem_type) is
-    variable ml  :line;
-		variable success : boolean;
-		variable v   : data_type;
-		variable i   : addr_type := 0 ;
-    begin 
-	     outest: loop   ----- read line by line
-	        exit when endfile  (Memoryfile);
-		      readline (f , ml);
-          
-          success := TRUE ;
-          ----read values in each line
-          while success loop 
-              read (ml, v, success);
-			    if success then
-              Memory (i) := v ;
-				  exit outest when
-				      i := 2**addr_width -1;
-			    end if;
-		      end loop;
-		   end loop;
-    end init_memory;
+    procedure init_memory(
+            variable Memoryfile : in text;
+            variable Memory : out mem_type ) is
+            variable ml : line;
+            variable success : boolean ;
+            variable v : data_type ;
+            variable i : addr_type := 0;
+        begin
+            outest: loop --- read line by line
+               exit when endfile (Memoryfile);
+               readline (f , ml);
+               
+               success := TRUE ;
+               -------read values in each line
+               while success loop
+                    read (ml, v, success)
+                    if success then
+                       Memory (i) := v;
+                       exit outest when
+                            i := 2 ** addr_width - 1;
+                     end if;
+                end if;
+                end loop;
+             end loop;
+        end init_memory;
 end mem_package_all;
