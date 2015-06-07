b8ad36ef60356f10395b59d11269f77429c082f2
flomaurer <flomaurer@users.noreply.github.com>
2015-06-07 21:13:54 +0200

    Assembler und funktionsfähig

--- mem_packagebody_all.vhd
+++ mem_packagebody_all.vhd
@@ -1,25 +1,32 @@
 package body mem_package_all is
-    procedure init_memory(
-            variable Memoryfile : in text;
-            variable Memory : out mem_type ) is
-          variable ml : line;
-          variable success : boolean ;
-          variable v : data_type ;
-          variable i : addr_type := 0;
+	procedure init_memory( Par: inout Boolean;  OP: inout data_type; variable Memoryfile : in text; variable Memory : out mem_type ) is
+
+        	variable ml : line;
+        	variable success : boolean ;
+        	variable v : data_type ;
+        	variable i : addr_type := 0;
+		-- variable dml: line;
+				
         begin
             outest:LOOP --- read line by line
                exit when endfile (Memoryfile);
                readline (Memoryfile , ml);
-               
                success := TRUE ;
 		-- read values in each line
 		while success LOOP
-			read(ml, v, success);
-			if success then
-				Memory(i):=v;
-				exit outest when
-					i=2**addr_width-1;
-				i:= i+1;
+			if Par then
+				read(ml, v, success);
+				if success then
+					Memory(i):=v;
+		exit outest when
+			i=2**addr_width-1;
+					i:= i+1;
+				end if;
+			else 
+				inputDecode(ml, Par, OP);
+				Memory(i):=OP;
+				success := False ;
+				i:=i+1;
 			end if;
 		end LOOP;
              end LOOP outest;
