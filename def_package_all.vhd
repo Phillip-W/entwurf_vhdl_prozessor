16758510016658bbdca6a595f99fe6ca043410a9
Sabbuth <ulrich.patrick@yahoo.com>
2015-06-12 01:39:15 +0200

    Update def_package_all.vhd

--- def_package_all.vhd
+++ def_package_all.vhd
@@ -110,15 +110,16 @@ PACKAGE def_package_all IS
 
   PROCEDURE SUBC (CONSTANT O1, O2 : IN data_type; R : INOUT data_type; C, Z, O, N : INOUT BOOLEAN);
 
--- Procedure XSLL(constant A: in data_type; variable R: out data_type; variable Z,CO,N,O: out boolean);
+  procedure XSLL(constant O1 : in data_type; R: out data_type; C: out boolean; O: out Boolean);
+  
+  procedure XSRL(constant O1: in data_type; R: out data_type; C: out boolean);
+
+  Procedure XSRA(constant O1: in data_type; R: out data_type; C: out boolean);
  
--- Procedure XSRA(constant A: in data_type; variable R: out data_type; variable Z,CO,N,O: out boolean);
+  procedure ROLC(constant O1: in data_type;  R: out data_type; CI : inout boolean);
  
--- procedure ROLC(constant A: in data_type; variable R: out data_type; variable Z: out boolean; variable C: inout boolean;
--- variable N,O: out boolean);
- 
--- procedure RORC(constant A: in data_type; variable R: out data_type; variable Z: out boolean; variable C: inout boolean;
--- variable N,O: out boolean);
+  procedure RORC(constant O1: in data_type;  R: out data_type; CI : inout boolean);
+  
   PROCEDURE ReadIn (Reg : OUT data_type);
   PROCEDURE WriteOut(CONSTANT Reg : IN data_type);
  
@@ -172,3 +173,4 @@ PACKAGE def_package_all IS
 -- PROCEDURE read_param (VARIABLE l : IN line; CONSTANT i_in : IN INTEGER; VARIABLE param: OUT String);
 
 END def_package_all;
+
